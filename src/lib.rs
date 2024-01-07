d344f5a611360f492ea985996fe4e6a47da7ec92
Stefan <42220813+Stefanuk12@users.noreply.github.com>
2024-01-07 18:31:34 +0000

    transition from `MaybeSignal<String>` to `MaybeProp<leptos::TextProp>`

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,13 @@ use leptos::*;
 mod generated;
 pub use generated::*;
 
+#[macro_export]
+macro_rules! maybe_text_prop_some {
+    ($e:expr) => {
+        ::leptos::MaybeProp::derive(move || Some(::leptos::TextProp::from($e)))
+    };
+}
+
 #[derive(Clone)]
 pub struct Path {
     pub path: fn() -> leptos::Fragment,
@@ -22,90 +29,98 @@ pub enum HeroIconsType {
     Mini,
 }
 
+/// `MaybeProp` might be cumbersome to use, but there is a helper macro [`maybe_text_prop_some!`] that can be used to simplify the usage.
+/// 
+/// Icon
 #[component]
 pub fn Icon(
     path: Path,
-    #[prop(into)]
-    #[prop(optional)]
-    class: MaybeSignal<String>,
 
     #[prop(into)]
     #[prop(optional)]
-    fill: MaybeSignal<String>,
+    class: MaybeProp<leptos::TextProp>,
 
     #[prop(into)]
     #[prop(optional)]
-    stroke_width: MaybeSignal<String>,
+    style: MaybeProp<leptos::TextProp>,
 
     #[prop(into)]
     #[prop(optional)]
-    stroke: MaybeSignal<String>,
+    fill: MaybeProp<leptos::TextProp>,
 
     #[prop(into)]
     #[prop(optional)]
-    width: MaybeSignal<String>,
+    stroke_width: MaybeProp<leptos::TextProp>,
 
     #[prop(into)]
     #[prop(optional)]
-    height: MaybeSignal<String>,
+    stroke: MaybeProp<leptos::TextProp>,
+
+    #[prop(into)]
+    #[prop(optional)]
+    width: MaybeProp<leptos::TextProp>,
+
+    #[prop(into)]
+    #[prop(optional)]
+    height: MaybeProp<leptos::TextProp>,
 ) -> impl IntoView {
-    match path.icon_type {
-        IconType::HeroIcons(HeroType) => match HeroType {
-            HeroIconsType::Outline => {
-                view! {
-                    <svg xmlns="http://www.w3.org/2000/svg" class=class.get() viewBox="0 0 24 24"
-                        data-license="From https://github.com/tailwindlabs/heroicons - Licensed under MIT"
-                        width=move || width.get().is_empty().then_some("24".to_string()).unwrap_or(width.get())
-                        height=move || height.get().is_empty().then_some("24".to_string()).unwrap_or(height.get())
-                        fill=move || fill.get().is_empty().then_some("none".to_string()).unwrap_or(fill.get())
-                        stroke-width=move || stroke_width.get().is_empty().then_some("2".to_string()).unwrap_or(stroke_width.get())
-                        stroke=move || stroke.get().is_empty().then_some("currentColor".to_string()).unwrap_or(stroke.get())
-                    >
-                        {(path.path)()}
-                    </svg>
-                }
-            }
-            HeroIconsType::Solid => {
-                view! {
-                    <svg xmlns="http://www.w3.org/2000/svg" class=class.get() viewBox="0 0 24 24"
-                        data-license="From https://github.com/tailwindlabs/heroicons - Licensed under MIT"
-                        width=move || width.get().is_empty().then_some("24".to_string()).unwrap_or(width.get())
-                        height=move || height.get().is_empty().then_some("24".to_string()).unwrap_or(height.get())
-                        fill=move || fill.get().is_empty().then_some("currentColor".to_string()).unwrap_or(fill.get())
-                        stroke-width=move || stroke_width.get().is_empty().then_some("2".to_string()).unwrap_or(stroke_width.get()) stroke=move || stroke.get().is_empty().then_some("currentColor".to_string()).unwrap_or(stroke.get())
-                    >
-                        {(path.path)()}
-                    </svg>
-                }
-            }
-            HeroIconsType::Mini => {
-                view! {
-                    <svg xmlns="http://www.w3.org/2000/svg" class=class.get() viewBox="0 0 20 20"
-                        data-license="From https://github.com/tailwindlabs/heroicons - Licensed under MIT"
-                        width=move || width.get().is_empty().then_some("20".to_string()).unwrap_or(width.get())
-                        height=move || height.get().is_empty().then_some("20".to_string()).unwrap_or(height.get())
-                        fill=move || fill.get().is_empty().then_some("currentColor".to_string()).unwrap_or(fill.get())
-                        stroke-width=move || stroke_width.get().is_empty().then_some("2".to_string()).unwrap_or(stroke_width.get()) stroke=move || stroke.get().is_empty().then_some("currentColor".to_string()).unwrap_or(stroke.get())
-                    >
-                        {(path.path)()}
-                    </svg>
 
+    let svg = move || {
+        let mut svg = leptos::svg::svg();
+
+        svg = svg.attr("xmlns", "http://www.w3.org/2000/svg");
+
+        if let Some(x) = class.get() {
+            svg = svg.attr("class", x);
+        }
+
+        if let Some(x) = style.get() {
+            svg = svg.attr("style", x);
+        }
+
+        if let Some(x) = width.get() {
+            svg = svg.attr("width", x);
+        }
+
+        if let Some(x) = height.get() {
+            svg = svg.attr("height", x);
+        }
+
+        if let Some(x) = fill.get() {
+            svg = svg.attr("fill", x);
+        }
+
+        if let Some(x) = stroke_width.get() {
+            svg = svg.attr("stroke-width", x);
+        }
+
+        if let Some(x) = stroke.get() {
+            svg = svg.attr("stroke", x);
+        }
+
+        match &path.icon_type {
+            IconType::HeroIcons(HeroType) => match HeroType {
+                HeroIconsType::Outline => {
+                    svg = svg.attr("data-license", "From https://github.com/tailwindlabs/heroicons - Licensed under MIT");
+                    svg = svg.attr("viewBox", "0 0 24 24");
+                },
+                HeroIconsType::Solid => {
+                    svg = svg.attr("data-license", "From https://github.com/tailwindlabs/heroicons - Licensed under MIT");
+                    svg = svg.attr("viewBox", "0 0 24 24");
+                },
+                HeroIconsType::Mini => {
+                    svg = svg.attr("data-license", "From https://github.com/tailwindlabs/heroicons - Licensed under MIT");
+                    svg = svg.attr("viewBox", "0 0 20 20");
                 }
-            }
-        },
-        IconType::Lucide => {
-            view! {
-                <svg xmlns="http://www.w3.org/2000/svg" class=class.get() viewBox="0 0 24 24"
-                    data-license="From https://github.com/lucide-icons/lucide - Licensed under ISC"
-                        width=move || width.get().is_empty().then_some("24".to_string()).unwrap_or(width.get())
-                        height=move || height.get().is_empty().then_some("24".to_string()).unwrap_or(height.get())
-                    fill=move || fill.get().is_empty().then_some("none".to_string()).unwrap_or(fill.get())
-                    stroke-width=move || stroke_width.get().is_empty().then_some("2".to_string()).unwrap_or(stroke_width.get()) stroke=move || stroke.get().is_empty().then_some("currentColor".to_string()).unwrap_or(stroke.get()) 
-                    stroke-linecap="round" stroke-linejoin="round"
-                >
-                    {(path.path)()}
-                </svg>
+            },
+            IconType::Lucide => {
+                svg = svg.attr("data-license", "From https://github.com/lucide-icons/lucide - Licensed under ISC");
+                svg = svg.attr("viewBox", "0 0 24 24");
             }
         }
-    }
+
+        svg.child((path.path)())
+    };
+
+    svg.into_view()
 }
